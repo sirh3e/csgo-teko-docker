f5724eb80428a2ff16bf58a74780e9a34b339251
sirh3e <sirh3e@protonmail.com>
2024-02-02 17:50:55 +0100

    Added csharp code

---
+++ src/Sirh3e.Teko.Docker/Sirh3e.Teko.Docker.API/Program.cs
@@ -0,0 +1,32 @@
+using Sirh3e.Teko.Docker.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddTransient<RpcClient>(configuration => new RpcClient("A"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (true)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/api/fib/{number}", async (ulong number) =>
+    {
+        var client = new RpcClient();
+        var message = await client.CallAsync(number.ToString());
+        
+        return ulong.Parse(message);
+    })
+    .WithName("GetFib")
+    .WithOpenApi();
+
+app.Run();
---
+++ src/Sirh3e.Teko.Docker/Sirh3e.Teko.Docker.Worker/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "database_timescale_rabbitmq",
+    UserName = "user",
+    Password = "password",
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "rpc_queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "rpc_queue",
+                     autoAck: false,
+                     consumer: consumer);
+
+consumer.Received += (model, ea) =>
+{
+    var response = string.Empty;
+
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var replyProps = channel.CreateBasicProperties();
+    replyProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        var message = Encoding.UTF8.GetString(body);
+        var n = ulong.Parse(message);
+        Console.WriteLine($" [.] Fib({message})");
+        response = Fib(n).ToString();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($" [.] {e.Message}");
+        response = string.Empty;
+    }
+    finally
+    {
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(exchange: string.Empty,
+                             routingKey: props.ReplyTo,
+                             basicProperties: replyProps,
+                             body: responseBytes);
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
+};
+
+await Task.Delay(Timeout.Infinite);
+
+// Assumes only valid positive integer input.
+// Don't expect this one to work for big numbers, and it's probably the slowest recursive implementation possible.
+static ulong Fib(ulong n)
+{
+    if (n < 2)
+        return n;
+    return Fib(n - 1) + Fib(n - 2);
+}
